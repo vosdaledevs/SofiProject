d6b21ee1a03ea6b81b84247aed3f858392226b9c
Ricardo René Chávez Ríos <rchavezrios@gmail.com>
2019-08-28 11:48:47 -0600

    HEADER y FOOTER VACIOS

--- view/dashboard.php
+++ view/dashboard.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+    <nav>
+
+    </nav>
 </head>
 <body>
     <h1><a href="../includes/logout.php">Logout</a></h1>
 </body>
+<footer>
+
+</footer>
 </html>
